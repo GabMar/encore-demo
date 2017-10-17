3d668b447ac590224ff1b62f4270b0a0fdb585a0
Gabriele Martini <gabrielemartini1990@gmail.com>
2017-10-17 11:19:37 +0200

    basic encore setup
    added basic libraries like sass-loader and bootstrap, andcreated a base template

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+    // directory where all compiled assets will be stored
+    .setOutputPath('web/build/')
+
+    // what's the public path to this directory (relative to your project's document root dir)
+    .setPublicPath('/build')
+
+    // empty the outputPath dir before each build
+    .cleanupOutputBeforeBuild()
+
+    // will output as web/build/app.js
+    .addEntry('app', './app/Resources/assets/js/main.js')
+
+    // will output as web/build/global.css
+    .addStyleEntry('global', './app/Resources/assets/css/global.scss')
+
+    // allow sass/scss files to be processed
+    .enableSassLoader()
+
+    // allow legacy applications to use $/jQuery as a global variable
+    .autoProvidejQuery()
+
+    .enableSourceMaps(!Encore.isProduction())
+;
+
+module.exports = Encore.getWebpackConfig();
